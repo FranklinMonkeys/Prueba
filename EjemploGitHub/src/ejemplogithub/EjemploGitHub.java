9bac587ef78f31072711e04ed86bfc21cb445bb8
Franklin A. Herrera Padilla <fahp@outlook.es>
2016-11-21 08:37:04 -0500

    Update EjemploGitHub.java

--- EjemploGitHub/src/ejemplogithub/EjemploGitHub.java
+++ EjemploGitHub/src/ejemplogithub/EjemploGitHub.java
@@ -3,8 +3,9 @@ package ejemplogithub;
 public class EjemploGitHub {
 
     public static void main(String[] args) {
-        System.out.println("Hola Mundo");//hugo
-        
+            System.out.println("Hola Mundo");//hugo
+        /* Franklin Herrera */
+    
     }
     
 }
